1f8251dcf745b6d7962b556b0e59535bd0d21b1f
Invazing <invazing@hotmail.com>
2021-06-28 10:10:02 -0300

    Adicionar .gitignore e .gitattributes.

---
+++ Cobranca/Form/Main.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.cod_cliente = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cod_cliente
+        '
+        Me.cod_cliente.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.cod_cliente.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cod_cliente.Location = New System.Drawing.Point(117, 19)
+        Me.cod_cliente.MaxLength = 4
+        Me.cod_cliente.Multiline = True
+        Me.cod_cliente.Name = "cod_cliente"
+        Me.cod_cliente.Size = New System.Drawing.Size(106, 28)
+        Me.cod_cliente.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 14.25!)
+        Me.Label1.Location = New System.Drawing.Point(6, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 22)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "CODIGO :"
+        '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.Document = Me.PrintDocument1
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'PrintDocument1
+        '
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Document = Me.PrintDocument1
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.cod_cliente)
+        Me.GroupBox1.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(1, 2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(231, 56)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GERADOR"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(233, 57)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Font = New System.Drawing.Font("Gigi", 8.25!)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Main"
+        Me.Text = "Gerar Envelope Cliente"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents cod_cliente As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Cobranca/Form/Main.vb
@@ -0,0 +1,46 @@
+﻿Imports FirebirdSql.Data.FirebirdClient
+Public Class Main
+    Dim conexaoFB As FbConnection
+    Dim da As FbDataAdapter
+    Dim ds As New DataSet
+    Dim id_cliente, nome, social, endereco, bairro, telefone, celular, cidade As String
+    Dim datacadastro As Date
+    Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+        e.Graphics.DrawString(id_cliente + " - " + nome, New Font("Arial", 14, FontStyle.Regular), Brushes.Black, 10, 10)
+        e.Graphics.DrawString("SOCIAL : " + social, New Font("Arial", 14, FontStyle.Regular), Brushes.Black, 10, 40)
+        e.Graphics.DrawString("END : " + endereco, New Font("Arial", 14, FontStyle.Regular), Brushes.Black, 10, 70)
+        e.Graphics.DrawString("BAIRRO : " + bairro, New Font("Arial", 14, FontStyle.Regular), Brushes.Black, 10, 100)
+        e.Graphics.DrawString("FONE : " + telefone, New Font("Arial", 14, FontStyle.Regular), Brushes.Black, 10, 130)
+        e.Graphics.DrawString("FONE : " + celular, New Font("Arial", 14, FontStyle.Regular), Brushes.Black, 10, 160)
+        e.Graphics.DrawString("CIDADE : " + cidade, New Font("Arial", 14, FontStyle.Regular), Brushes.Black, 10, 190)
+        e.Graphics.DrawString("DATA CADASTRO : " + datacadastro, New Font("Arial", 14, FontStyle.Regular), Brushes.Black, 10, 220)
+    End Sub
+    Private Sub Cobranca_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown, cod_cliente.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            conexaoFB.Open()
+            Try
+                da = New FbDataAdapter("SELECT GE_pessoa.id_pessoa AS COD_CLIENTE, GE_PESSOA.razao AS NOME, GE_PESSOA.fantasia AS SOCIAL, GE_PESSOA.data_cadastro AS CADASTRO, GE_PESSOA_ENDERECO.endereco AS ENDERECO, GE_PESSOA_ENDERECO.bairro AS BAIRRO, GE_PESSOA_ENDERECO.telefone AS TELEFONE, GE_PESSOA_ENDERECO.celular AS CELULAR, GE_CIDADE.nome AS CIDADE FROM GE_PESSOA INNER JOIN GE_PESSOA_ENDERECO ON GE_PESSOA_ENDERECO.id_pessoa = GE_PESSOA.id_pessoa INNER JOIN GE_CIDADE ON GE_CIDADE.id_cidade = GE_PESSOA_ENDERECO.id_cidade WHERE GE_PESSOA.id_pessoa='" & cod_cliente.Text & "'", conexaoFB)
+                da.Fill(ds, "Cadastro")
+                Dim dt As DataTable = New DataTable
+                da.Fill(dt)
+                id_cliente = dt.Rows(0)(0).ToString
+                nome = dt.Rows(0)(1).ToString
+                social = dt.Rows(0)(2).ToString
+                datacadastro = dt.Rows(0)(3).ToString
+                endereco = dt.Rows(0)(4).ToString
+                bairro = dt.Rows(0)(5).ToString
+                telefone = dt.Rows(0)(6).ToString
+                celular = dt.Rows(0)(7).ToString
+                cidade = dt.Rows(0)(8).ToString
+                PrintDocument1.Print()
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+            cod_cliente.Text = ""
+            conexaoFB.Close()
+        End If
+    End Sub
+    Private Sub Cobranca_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conexaoFB = New FbConnection("Server=localhost;User=SYSDBA;Password=masterkey;Database=C:\NTS\SIGEWIN\SIGE.FDB")
+    End Sub
+End Class
